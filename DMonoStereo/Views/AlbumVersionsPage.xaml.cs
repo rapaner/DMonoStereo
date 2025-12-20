36b21692e85c52e838b69c707878dc4bf87db5b1
rapaner <rapaner@yandex.ru>
2025-12-20 09:23:55 +0300

    Обновлены пакеты

--- DMonoStereo/Views/AlbumVersionsPage.xaml.cs
+++ DMonoStereo/Views/AlbumVersionsPage.xaml.cs
@@ -468,7 +468,7 @@ public partial class AlbumVersionsPage : ContentPage
                 return;
             }
 
-            await DisplayAlert("Ошибка", $"Введите число от 1 до {_totalPages}.", "OK");
+            await DisplayAlertAsync("Ошибка", $"Введите число от 1 до {_totalPages}.", "OK");
         }
     }
 }
